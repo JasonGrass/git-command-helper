6fbe0fe791b07ee5ae01a9eb16c2dfa8506e332e
JasonGrass <junjiegrass@qq.com>
2021-11-26 18:26:29 +0800

    简化配置文件的引用

--- src/my.config.js
+++ src/my.config.js
@@ -1,13 +1,3 @@
-const path = require('path')
-const fs = require("fs");
-const file = path.resolve(__dirname, "my.config.secret.js");
-
-fs.exists(file, exists => {
-    if (!exists) {
-        throw new Error("没有找到配置文件 my.config.secret.js, 请添加")
-    }
-})
-
 module.exports = require("./my.config.secret")
 
 /*
